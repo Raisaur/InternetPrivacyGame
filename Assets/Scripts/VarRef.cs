44b7515d4f9681c60a3aa77982f1377163b7a545
Ella <ella.fernstrom@gmail.com>
2020-03-04 10:27:54 +0100

    number labels for tags and topics

--- Assets/Scripts/VarRef.cs
+++ Assets/Scripts/VarRef.cs
@@ -7,71 +7,71 @@ public class VarRef : MonoBehaviour
     public enum Tag
     {
         //Gender
-        GenWoman = 0,
-        GenMan,
+        GenWoman = 0,       //0
+        GenMan,             //1
 
         //Age
-        AgeChild,
-        AgeAdult,
-        AgeElderly,
+        AgeChild,           //2
+        AgeAdult,           //3
+        AgeElderly,         //4
 
         //Living location
-        LiveCity,
-        LiveCountry,
+        LiveCity,           //5
+        LiveCountry,        //6
 
         //Occupation
-        OccStudent,
-        OccEmployed,
-        OccUnemployed,
+        OccStudent,         //7
+        OccEmployed,        //8
+        OccUnemployed,      //9
 
         //Family status
-        FamSingle,
-        FamInRelationship,
-        FamHasKids,
+        FamSingle,          //10
+        FamInRelationship,  //11
+        FamHasKids,         //12
 
         //Financial status
-        FinPoor,
-        FinMiddleClass,
-        FinRich,
+        FinPoor,            //13
+        FinMiddleClass,     //14
+        FinRich,            //15
 
         //Most searched
-        SearchFashion,
-        SearchParty,
-        SearchOutdoor,
-        SearchEatOut,
-        SearchVacation,
-        SearchIndoor,
-        SearchBusiness,
-        SearchVehicle,
+        SearchFashion,      //16
+        SearchParty,        //17    
+        SearchOutdoor,      //18
+        SearchEatOut,       //19
+        SearchVacation,     //20
+        SearchIndoor,       //21
+        SearchBusiness,     //22
+        SearchVehicle,      //23
 
         //No tag
-        blank
+        blank               //24
     }
 
     public enum Topic
     {
-        TravelCruise = 0,
-        TravelCharter,
-        TravelBudget,
+        TravelCruise = 0,   //0
+        TravelCharter,      //1
+        TravelBudget,       //2
 
-        ElecPhone,
-        ElecGames,
-        ElecHouse,
+        ElecPhone,          //3
+        ElecGames,          //4
+        ElecHouse,          //5
 
-        SportsFootball,
-        SportsSwimwear,
-        SportsHike,
+        SportsFootball,     //6
+        SportsSwimwear,     //7
+        SportsHike,         //8
 
-        FoodQuick,
-        FoodGourmet,
-        FoodCafe,
+        FoodQuick,          //9
+        FoodGourmet,        //10
+        FoodCafe,           //11
 
-        CarsBudget,
-        CarsHighend,
-        CarsParts,
+        CarsBudget,         //12
+        CarsHighend,        //13
+        CarsParts,          //14
 
-        ClothesWomen,
-        ClothesMen,
-        ClothesCostume
+        ClothesWomen,       //15
+        ClothesMen,         //16
+        ClothesCostume      //17
     }
 }
