afa3b9ed5c0cc62a755bc00f5aab12196d7bd489
Ella <ella.fernstrom@gmail.com>
2020-03-04 10:49:40 +0100

    randomizer in profile manager

--- Assets/Scripts/ProfileManager.cs
+++ Assets/Scripts/ProfileManager.cs
@@ -2,6 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TagCategory
+{
+    Gender = 0,
+    Age,
+    Location,
+    Occupation,
+    Family,
+    Financial,
+    Searched
+}
+
+
 public class ProfileManager : MonoBehaviour
 {
     List<ProfileBase> profiles;
@@ -9,16 +21,55 @@ public class ProfileManager : MonoBehaviour
 
     void Start()
     {
-        //Order: gender, age, location, occupation, family, financial, searched
-        NewProfile = new ProfileBase(VarRef.Tag.GenWoman, VarRef.Tag.AgeAdult, VarRef.Tag.LiveCity,
-                                     VarRef.Tag.OccStudent, VarRef.Tag.FamSingle,
-                                     VarRef.Tag.FinMiddleClass, VarRef.Tag.SearchFashion);
-        profiles.Add(NewProfile);
+        for (int i = 0; i < 120; i++)
+        {
+            //Order: gender, age, location, occupation, family, financial, searched
+            NewProfile = new ProfileBase(RandTag(TagCategory.Gender), RandTag(TagCategory.Age), RandTag(TagCategory.Location),
+                                         RandTag(TagCategory.Occupation), RandTag(TagCategory.Family),
+                                         RandTag(TagCategory.Financial), RandTag(TagCategory.Searched));
+            profiles.Add(NewProfile);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    VarRef.Tag RandTag(TagCategory category)
+    {
+        int min = 0;
+        int max = 0;
+
+        switch (category)
+        {
+            case TagCategory.Gender:
+                min = 0; max = 1;
+                break;
+            case TagCategory.Age:
+                min = 2; max = 4;
+                break;
+            case TagCategory.Location:
+                min = 5; max = 6;
+                break;
+            case TagCategory.Occupation:
+                min = 7; max = 9;
+                break;
+            case TagCategory.Family:
+                min = 10; max = 12;
+                break;
+            case TagCategory.Financial:
+                min = 13; max = 15;
+                break;
+            case TagCategory.Searched:
+                min = 16; max = 23;
+                break;
+            default:
+                break;
+        }
+        int rand = Random.Range(min, max);
+        VarRef.Tag ret = (VarRef.Tag)rand;
+
+        return ret;
     }
 }
